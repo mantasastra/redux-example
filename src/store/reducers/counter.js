0aaeb63f3d36e5b350b8c46967c805de621b88f2
Mantas Astrauskas <mastrauskas4@gmail.com>
2020-07-22 16:35:31 +0100

    Refactor reducers

--- src/store/reducers/counter.js
+++ src/store/reducers/counter.js
@@ -1,4 +1,5 @@
 import * as actionTypes from "../actions/actionTypes";
+import { updateObject } from "../utility";
 
 const initialState = {
   counter: 0,
@@ -7,31 +8,25 @@ const initialState = {
 const counter = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.INCREMENT: {
-      return {
-        ...state,
-        counter: state.counter + 1,
-      };
+      return updateObject(state, { counter: state.counter + 1 });
     }
 
     case actionTypes.DECREMENT: {
-      return {
-        ...state,
+      return updateObject(state, {
         counter: state.counter - 1,
-      };
+      });
     }
 
     case actionTypes.ADD: {
-      return {
-        ...state,
+      return updateObject(state, {
         counter: state.counter + action.value,
-      };
+      });
     }
 
     case actionTypes.SUBTRACT: {
-      return {
-        ...state,
+      return updateObject(state, {
         counter: state.counter - action.value,
-      };
+      });
     }
     default:
       return state;
--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -1,22 +1,32 @@
 import * as actionTypes from "../actions/actionTypes";
+import { updateObject } from "../utility";
 
 const initialState = {
   results: [],
 };
 
+const storeResult = (state, action) => {
+  const results = state.results.concat({
+    id: new Date(),
+    value: action.result,
+  });
+
+  return updateObject(state, { results });
+};
+
+const deleteResult = (state, action) => {
+  const results = state.results.filter((result) => result.id !== action.id);
+
+  return updateObject(state, { results });
+};
+
 const result = (state = initialState, action) => {
   if (action.type === actionTypes.STORE_RESULT) {
-    return {
-      ...state,
-      results: state.results.concat({ id: new Date(), value: action.result }),
-    };
+    return storeResult(state, action);
   }
 
   if (action.type === actionTypes.DELETE_RESULT) {
-    return {
-      ...state,
-      results: state.results.filter((result) => result.id !== action.resultId),
-    };
+    return deleteResult(state, action);
   }
 
   return state;
