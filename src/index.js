853dd248a50d1e3a9ee2575618d59b6a9838adf4
Mantas Astrauskas <mastrauskas4@gmail.com>
2020-07-22 12:04:19 +0100

    Add logger middleware and connect redux dev tools

--- src/index.js
+++ src/index.js
@@ -1,13 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
+
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 import rootReducer from "./store/reducers";
 
-const store = createStore(rootReducer);
+const logger = (store) => {
+  return (next) => {
+    return (action) => {
+      console.log("[MIDDLEWARE] Dispatching", action);
+
+      const result = next(action);
+
+      console.log("[MIDDLEWARE] Next state", store.getState());
+
+      return result;
+    };
+  };
+};
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(logger))
+);
 
 ReactDOM.render(
   <Provider store={store}>
